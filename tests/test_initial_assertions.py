84d2c49c3c4cc6236eb87f9822e5db57fe44f78f
Gerardo Herrera <gherreral1337@gmail.com>
2026-03-10 17:04:57 -0600

    Add more tests - parametrize, fixtures

--- tests/test_initial_assertions.py
+++ tests/test_initial_assertions.py
@@ -1,5 +1,27 @@
+import pytest
+
+@pytest.mark.sanity
 def test_01():
     assert 5 + 5 == 10
 
+@pytest.mark.smoke
 def test_02():
     assert "Hello" == "Hello"
+
+#PARAMETRIZE
+@pytest.mark.parametrize("test_input", [82, 10, 0, 5, 6])
+def test_param01(test_input):
+    assert test_input > 50
+
+
+# FIXTURES
+@pytest.fixture()
+def setup_list() -> str:
+    print("\n Fixtures ...  ")
+    city = ["New York", "London", "Riyadh", "Mexico City"]
+    return city
+
+def test_using_fixtures(setup_list):
+    print(setup_list[:])
+    assert setup_list[0] == "New York"
+    assert setup_list[::2] == ["New York", "Riyadh"]
